1e1c580d517fdd79334b15b32281479aa01ecb93
Jakub Sedlacek <sedlaj50@fit.cvut.cz>
2026-06-09 22:38:49 +0200

    Add legal stuff

--- personalhq/routes/landing.py
+++ personalhq/routes/landing.py
@@ -6,3 +6,11 @@ landing_bp = Blueprint('landing', __name__)
 @landing_bp.route('/')
 def landing():
     return render_template('landing.html')
+
+@landing_bp.route('/terms')
+def terms():
+    return render_template('legal/terms.html')
+
+@landing_bp.route('/privacy')
+def privacy():
+    return render_template('legal/privacy.html')
