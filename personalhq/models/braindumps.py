95609901178bc2cc11d3811c59e7b8349267fda7
Jakub Sedlacek <sedlaj50@fit.cvut.cz>
2026-06-15 21:20:05 +0200

    Encrypt using AES

--- personalhq/models/braindumps.py
+++ personalhq/models/braindumps.py
@@ -1,16 +1,17 @@
 """Module defining SQLAlchemy model of BrainDumps."""
 
-#import os
+import os
 from datetime import datetime
 from flask import current_app
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy_utils import StringEncryptedType
-from sqlalchemy_utils.types.encrypted.encrypted_type import FernetEngine
+#from sqlalchemy_utils.types.encrypted.encrypted_type import FernetEngine
+from sqlalchemy_utils.types.encrypted.encrypted_type import AesEngine
 from personalhq.extensions import db
 
-#def get_encryption_key():
-#    return os.environ.get('ENCRYPTION_KEY') or current_app.config.get('ENCRYPTION_KEY')
+def get_encryption_key():
+    return os.environ.get('ENCRYPTION_KEY') or current_app.config.get('ENCRYPTION_KEY')
 
 class BrainDump(db.Model):  # pylint: disable=R0903; # sqlalchemy class used to only store data
     """Class representing table brain_dumps in database."""
@@ -20,11 +21,15 @@ class BrainDump(db.Model):  # pylint: disable=R0903; # sqlalchemy class used to 
     user_id: Mapped[int] = mapped_column(ForeignKey('users.id'), nullable=False)
 
 # Inactive due to issues with InvalidToken when building from image
+# Fernet is very sensitive to data transfer - when it travels through docker hub, there might be some padding issues
 #    content: Mapped[str] = mapped_column(
 #        StringEncryptedType(db.Text, get_encryption_key, FernetEngine),
 #        nullable=False
 #    )
-    content: Mapped[str] = mapped_column(nullable=False)
+    content: Mapped[str] = mapped_column(
+        StringEncryptedType(db.Text, get_encryption_key, AesEngine, 'pkcs5'),
+        nullable=False
+    )
     created_at: Mapped[datetime] = mapped_column(nullable=False)
     processed: Mapped[bool | None]
     
--- personalhq/models/journalentries.py
+++ personalhq/models/journalentries.py
@@ -1,17 +1,18 @@
 """Module defining SQLAlchemy models for Journal entries."""
 
-#import os
+import os
 from datetime import datetime
 from flask import current_app
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy_utils import StringEncryptedType
-from sqlalchemy_utils.types.encrypted.encrypted_type import FernetEngine
+#from sqlalchemy_utils.types.encrypted.encrypted_type import FernetEngine
+from sqlalchemy_utils.types.encrypted.encrypted_type import AesEngine
 from personalhq.services.time_service import get_utc_now
 from personalhq.extensions import db
 
-#def get_encryption_key():
-#    return os.environ.get('ENCRYPTION_KEY') or current_app.config.get('ENCRYPTION_KEY')
+def get_encryption_key():
+    return os.environ.get('ENCRYPTION_KEY') or current_app.config.get('ENCRYPTION_KEY')
 
 class JournalEntry(db.Model):
     """Class representing a single, timestamped entry within a journal."""
@@ -22,11 +23,15 @@ class JournalEntry(db.Model):
     prompt_id: Mapped[int] = mapped_column(ForeignKey('journal_prompts.id', ondelete='SET NULL'), nullable=True)
 
 # Inactive due to issues with InvalidToken when building from image
+# Fernet is very sensitive to data transfer - when it travels through docker hub, there might be some padding issues
 #    content: Mapped[str] = mapped_column(
 #        StringEncryptedType(db.Text, get_encryption_key, FernetEngine),
 #        nullable=False
 #    )
-    content: Mapped[str] = mapped_column(nullable=False)
+    content: Mapped[str] = mapped_column(
+        StringEncryptedType(db.Text, get_encryption_key, AesEngine, 'pkcs5'),
+        nullable=False
+    )
     created_at: Mapped[datetime] = mapped_column(default=get_utc_now)
 
     ai_insight: Mapped[str | None]
